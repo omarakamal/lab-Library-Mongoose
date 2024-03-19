52c29acf5e66f8f984cd2f1adb3d05a9b1ad8b99
moreira8125 <your_github_email>
2024-03-19 20:42:01 +0000

    lab done

---
+++ models/Book.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  pages: {
+    type: Number,
+    min: 20,
+    max: 1000,
+  },
+  cover: {
+    type: String,
+    default:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Open_book_nae_02.svg/800px-Open_book_nae_02.svg.png",
+  },
+  genre: [String],
+  isBestSeller: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
