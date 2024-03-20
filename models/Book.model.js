a0dd070282c89617d250e1b6d77c30ead2c15eb0
Mohammed Ashour <mashour8@gmail.com>
2024-03-20 21:43:51 +0100

    done

---
+++ models/Book.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, require: true },
+  author: { type: String, require: true },
+  pages: { type: Number, min: 20, max: 1000 },
+  cover: {
+    type: String,
+    default:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Open_book_nae_02.svg/800px-Open_book_nae_02.svg.png",
+  },
+  genres: { type: [String] },
+  isBestSeller: { type: Boolean, default: false },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
